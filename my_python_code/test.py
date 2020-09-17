b45c665293dfe91a49f735c16d5ab55139e46ca5
Robert Schwyzer <robert.s@hostdime.com>
2020-09-16 20:53:40 -0400

    Added support for setting `test.py` connection parameters via ENV

--- my_python_code/test.py
+++ my_python_code/test.py
@@ -1,13 +1,21 @@
 # Module Import
 import mariadb
 import sys
+import os
+
+# Pull defaults from OS environment if they exist
+user = os.environ.get('MARIADB_USER', 'connpy_test')
+password = os.environ.get('MARIADB_PASS', 'passwd')
+host = os.environ.get('MARIADB_HOST', 'localhost')
+port = int(os.environ.get('MARIADB_PORT', 3306))
+
 # Instantiate Connection
 try:
    conn = mariadb.connect(
-      user="connpy_test",
-      password="passwd",
-      host="localhost",
-      port=3306)
+      user=user,
+      password=password,
+      host=host,
+      port=port)
 except mariadb.Error as e:
    print(f"Error connecting to MariaDB Platform: {e}")
    sys.exit(1)
